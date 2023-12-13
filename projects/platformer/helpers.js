fa63fd68a5314381aa7eab8275975b1e813d3687
Corlecia White <cwhite25@livingschoolnola.org>
2023-12-13 20:37:44 +0000

    yuh 2

--- projects/platformer/helpers.js
+++ projects/platformer/helpers.js
@@ -411,7 +411,7 @@ function playerFrictionAndGravity() {
 
 function drawPlatforms() {
   for (var i = 0; i < platforms.length; i++) {
-    ctx.fillStyle = "#880D0D";
+    ctx.fillStyle = "#0BDA51";
     ctx.fillRect(
       platforms[i].x,
       platforms[i].y,
